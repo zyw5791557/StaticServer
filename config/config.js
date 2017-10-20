9f6fca913041f4a6f802ad205bd28c17a56ffba9
Emlice <xiaomozyw@sina.com>
2017-10-20 22:08:55 +0800

    配置测试服务器和线上服务器

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
 	AvatarPath: '/assets/images/users/',
-	STATIC_SERVER: 'http://localhost:8989',
+	// STATIC_SERVER: 'http://localhost:8989',			// 本地测试地址
+	STATIC_SERVER: 'http://static.emlice.top'			// 服务器地址
 }
