e8f2a3cc56977bdfeeb5a155007a9752f6cfcca3
Amo <xiaomozyw@sina.com>
2017-10-23 18:10:11 +0800

    1

--- index.js
+++ index.js
@@ -19,12 +19,13 @@ mongoose.Promise = global.Promise;
 // 上传文件配置地址
 var config = require('./config/config.js');
 var AvatarPath_BASE = __dirname + config.AvatarPath;
+var MessageImgPath_BASE = __dirname + config.MessageImgPath;
 
 // 静态资源服务器地址配置
 var STATIC_SERVER = config.STATIC_SERVER;
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -35,6 +36,7 @@ app.use(allowCrossDomain);
 
 app.use("/", express.static(__dirname + "/assets"));
 
+// 头像上传
 app.post('/api/avatar_upload', upload.single('avatar'), function(req,res,next) {
 	// 设置编码格式
 	req.header('Content-Type', 'charset=utf-8');
@@ -64,6 +66,31 @@ app.post('/api/avatar_upload', upload.single('avatar'), function(req,res,next) {
 					}
 				});
 				res.send({ Code: 0, Str: '文件上传成功！', Avatar: remoteAvatar });
+			}
+		});
+	}
+});
+
+// 截图上传
+app.post('/api/ps_upload', upload.single('ps'), function(req,res,next) {
+	// 设置编码格式
+	req.header('Content-Type', 'charset=utf-8');
+	if(req.file) {
+		// 谁上传的  req.body.avatarName
+		var name = req.body.avatarName;
+		// 获取时间戳
+		var date = Date.now();
+		// 存哪里, 取什么名字。
+		var fileFormat = req.file.originalname.split('.');
+		// 远程访问地址
+		var remoteAvatar = STATIC_SERVER + '/images/printscreen/' + 'message_' + date + '.' + fileFormat[fileFormat.length - 1];
+		var avatarName = MessageImgPath_BASE + 'message_' + date + '.' + fileFormat[fileFormat.length - 1];
+		// 写入磁盘
+		fs.writeFile(avatarName, req.file.buffer, function(err) {
+			if(err) {
+				res.send({ Code: -1, Str: '文件上传失败！' });
+			} else {
+				res.send({ Code: 0, Str: '文件上传成功！', ps: remoteAvatar });
 			}
 		});
 	}
