d44433cddbc48cfd5f45cc2cce3ec505d24be5d6
Amo <xiaomozyw@sina.com>
2017-10-25 17:53:30 +0800

    1

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var MessageImgPath_BASE = __dirname + config.MessageImgPath;
 var STATIC_SERVER = config.STATIC_SERVER;
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
